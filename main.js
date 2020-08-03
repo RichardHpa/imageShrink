4bcff3cb441886dacff4a8ee6da31b1e51392bae
RichardHpa <richard.hpa@hotmail.com>
2020-08-03 22:00:22 +1200

    added logs for project

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const imagemin = require('imagemin')
 const imageminMozJpeg = require('imagemin-mozjpeg')
 const imageminPngquant = require('imagemin-pngquant')
 const slash = require('slash')
+const log = require('electron-log')
 const {
     app,
     BrowserWindow,
@@ -14,7 +15,7 @@ const {
 } = require('electron')
 
 // Set env
-process.env.NODE_ENY = 'development'
+process.env.NODE_ENV = 'production'
 
 const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
@@ -157,12 +158,13 @@ async function shrinkImage({ imgPath, quality, dest }) {
                 })
             ]
         })
-        console.log(files)
+
+        log.info(files)
         shell.openPath(dest)
 
         mainWindow.webContents.send('image:done')
     } catch (err) {
-        console.log(err)
+        log.error(err)
     }
 }
 
